feab5e790be610e27e5e0d453e945ef6274f8b28
andreas64 <derzandrr03@gmail.com>
2024-07-02 02:05:26 +0700

    add new textTheme scheme 'titleSmall'

--- lib/frontend/themes/light_theme.dart
+++ lib/frontend/themes/light_theme.dart
@@ -7,35 +7,29 @@ ThemeData light_theme = ThemeData(
   scaffoldBackgroundColor: Colors.white,
   useMaterial3: true,
   textTheme: const TextTheme(
-    displaySmall: TextStyle(
-      fontFamily: "Quicksand",
-      fontSize: 14, 
-      color: Colors.white
-    ),
+    displaySmall:
+        TextStyle(fontFamily: "Quicksand", fontSize: 14, color: Colors.white),
     displayMedium: TextStyle(
-      fontFamily: "Quicksand",
-      fontSize: 18, 
-      fontWeight: FontWeight.w600,
-      color: Colors.white
-    ),
+        fontFamily: "Quicksand",
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: Colors.white),
     titleMedium: TextStyle(
-      fontFamily: "Quicksand",
-      fontSize: 22, 
-      fontWeight: FontWeight.w600,
-      color: black_font_color
-    )
+        fontFamily: "Quicksand",
+        fontSize: 22,
+        fontWeight: FontWeight.w600,
+        color: black_font_color),
+    titleSmall: TextStyle(
+        fontFamily: "Quicksand",
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+        color: Colors.white),
   ),
   appBarTheme: const AppBarTheme(
     centerTitle: true,
     color: Colors.white,
-    shape: Border(
-      bottom: BorderSide(
-        color: Colors.grey,
-        width: 0.35
-      )
-    ),
+    shape: Border(bottom: BorderSide(color: Colors.grey, width: 0.35)),
     iconTheme: IconThemeData(color: black_font_color),
   ),
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-  
 );

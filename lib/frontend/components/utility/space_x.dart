5ccb80bde10c6a5c7c8cc203cdf4360185684225
andreas64 <derzandrr03@gmail.com>
2024-07-02 18:14:03 +0700

    add new utility space_x.dart

--- lib/frontend/components/utility/space_x.dart
+++ lib/frontend/components/utility/space_x.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 double vw(BuildContext context, double percentage) {
   return MediaQuery.of(context).size.width * (percentage / 100);
 }
+
+Widget dummyWidth(double w){
+  return Container(width: w,);
+}

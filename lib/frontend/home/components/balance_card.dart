433b2cb6016e2e20a1feb5465e2ed12a7a140a20
andreas64 <derzandrr03@gmail.com>
2024-07-02 18:15:37 +0700

    add /home widgets: balance and income card

---
+++ lib/frontend/home/components/balance_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:keuanganku/frontend/components/cards/k_card.dart';
+import 'package:keuanganku/frontend/components/utility/currency_format.dart';
+
+class BalanceCard extends StatefulWidget {
+  final String? name;
+  final double? balance;
+
+  const BalanceCard({super.key, this.name, this.balance});
+
+  @override
+  State<BalanceCard> createState() => _BalanceCardState();
+}
+
+class _BalanceCardState extends State<BalanceCard> {
+  @override
+  Widget build(BuildContext context) {
+    return KCard(
+      _child(context, name: widget.name, balance: widget.balance),
+      const Color(0xff363658),
+    );
+  }
+
+  Widget _child(BuildContext context, {String? name, double? balance}) {
+    name = name ?? 'John';
+    balance = balance ?? 0;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hi',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(
+              name,
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              'Balance',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(
+              currencyFormat(balance),
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/frontend/home/components/income_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:keuanganku/frontend/components/buttons/k_button.dart';
+import 'package:keuanganku/frontend/components/cards/k_card_plus.dart';
+import 'package:keuanganku/frontend/components/utility/currency_format.dart';
+import 'package:keuanganku/frontend/utility/color.dart';
+
+class IncomeCard extends StatefulWidget {
+  const IncomeCard({super.key});
+  final bg_color = const Color(0xff489F7B);
+
+  @override
+  State<IncomeCard> createState() => _IncomeCardState();
+}
+
+class _IncomeCardState extends State<IncomeCard> {
+
+  Widget content(BuildContext context) {
+    List<Color> generated3color = generate3Color(widget.bg_color);
+
+    return (Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Income this month',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(
+              currencyFormat(1200000),
+              style: Theme.of(context).textTheme.displayMedium,
+            )
+          ],
+        ),
+        k_button(context, (){}, icon: Icons.add, text: 'Add', mainColor: generated3color[1], iconColor: Colors.white)
+      ],
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KCardPlus(context, content(context),
+        title: 'Income', color: widget.bg_color);
+  }
+}

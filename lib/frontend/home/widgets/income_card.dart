daf9cc1310a257a1862f25fec5f3780021ff6438
andreas64 <derzandrr03@gmail.com>
2024-07-03 20:22:02 +0700

    add Dropdown and widget data class

--- lib/frontend/home/widgets/income_card.dart
+++ lib/frontend/home/widgets/income_card.dart
@@ -1,21 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:keuanganku/frontend/components/buttons/k_button.dart';
 import 'package:keuanganku/frontend/components/cards/k_card_plus.dart';
+import 'package:keuanganku/frontend/components/dropdown/k_dropdown.dart';
+import 'package:keuanganku/frontend/components/enum/date_range.dart';
 import 'package:keuanganku/frontend/components/utility/currency_format.dart';
 import 'package:keuanganku/frontend/utility/color.dart';
 
+class IncomeCardData {
+  DateRange dateRangeValue;
+
+  set dateRange(DateRange val) {
+    dateRangeValue = val;
+  }
+
+  IncomeCardData({required this.dateRangeValue});
+}
+
 class IncomeCard extends StatefulWidget {
-  const IncomeCard({super.key});
+  const IncomeCard(this.income_card_data, {super.key});
+
   final bg_color = const Color(0xff489F7B);
+  final IncomeCardData income_card_data;
 
   @override
   State<IncomeCard> createState() => _IncomeCardState();
 }
 
 class _IncomeCardState extends State<IncomeCard> {
+  void onDropdownDateRangeChange(DateRange? val) {
+    setState(() {
+      if (val != null) {
+        widget.income_card_data.dateRange = val;
+      }
+    });
+  }
+
   Widget content(BuildContext context) {
-    List<Color> generated3color = generate3Color(widget.bg_color);
+    final KDropdown<DateRange> data_range =
+        KDropdown(KDropdownItem( DateRange.weekly.getDateRangeMap()));
+    final List<Color> generated3color = generate3Color(widget.bg_color);
 
     return (Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -24,6 +47,13 @@ class _IncomeCardState extends State<IncomeCard> {
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            data_range.dropdownButton(
+              widget.income_card_data.dateRangeValue,
+              onDropdownDateRangeChange,
+              icon_theme: Theme.of(context).iconTheme,
+              dropdown_bg_color: generated3color[1],
+              text_style:  Theme.of(context).textTheme.labelMedium,
+            ),
             Text(
               'Income this month',
               style: Theme.of(context).textTheme.titleSmall,

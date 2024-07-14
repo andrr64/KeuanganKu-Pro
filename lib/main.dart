232f22f8d74f4d3382e5b7d09cd23a3a87936495
andreas64 <derzandrr03@gmail.com>
2024-07-15 05:51:55 +0700

    add database init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:keuanganku/backend/database/database_services.dart';
 import 'package:keuanganku/frontend/home/home.dart';
 import 'package:keuanganku/frontend/themes/light_theme.dart';
 
-void main() {
+DatabaseServices db = DatabaseServices();
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await db.openDb();
   runApp(const MyApp());
 }
 

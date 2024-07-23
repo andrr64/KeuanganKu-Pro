51aae5f4d176bbd042af1d61cd313df6ff13990f
andreas64 <derzandrr03@gmail.com>
2024-07-23 21:49:46 +0700

    add ErrorPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:keuanganku/backend/database/database_services.dart';
 import 'package:keuanganku/frontend/app/home/home.dart';
+import 'package:keuanganku/frontend/app/pages/content_when_x.dart';
 import 'package:keuanganku/frontend/themes/light_theme.dart';
 
 DatabaseServices db = DatabaseServices();
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await db.openDb(); // open database, and wait..
+  try {
+    await db.openDb();
+  } catch(E){
+    runApp(ErrPage(e: E));
+    return;
+  }
   runApp(const MyApp()); // if the database successfully opened, run the App
+}
+
+class ErrPage extends StatelessWidget {
+  const ErrPage({super.key, required this.e});
+  final Object e;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Error',
+      home: contentWhenError(context, '$e'),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {

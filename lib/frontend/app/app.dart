cd64026d8d0847fec08fbb69d8e1ed950b2c1a3b
andreas64 <derzandrr03@gmail.com>
2024-07-31 22:22:10 +0700

    use KeepAlivePage and update BottomNavBar desain

--- lib/frontend/app/app.dart
+++ lib/frontend/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:keuanganku/frontend/app/expense_category_provider.dart';
@@ -5,35 +6,60 @@ import 'package:keuanganku/frontend/app/home/home.dart';
 import 'package:keuanganku/frontend/app/home/home_provider.dart';
 import 'package:keuanganku/frontend/app/income_category_provider.dart';
 import 'package:keuanganku/frontend/app/wallet_provider.dart';
+import 'package:keuanganku/frontend/components/text/k_text.dart';
 import 'package:keuanganku/frontend/utility/k_color.dart';
+import 'package:keuanganku/frontend/utility/keep_alive.dart';
 
 final pageIndexProvider = StateProvider<int>((_) => 0);
-final pageControllerProvider = StateProvider<PageController>((_) => PageController());
-final mainPageScaffoldKeyProvider = StateProvider<GlobalKey<ScaffoldState>>((_) => GlobalKey<ScaffoldState>());
-final pageNames = ["Home", "Wallet"];
-final pagesProvider = StateProvider<List<Widget>>((ref) {
-  return [
-    const Homepage(),
-    const Homepage(),
-  ];
-});
+final pageControllerProvider =
+    StateProvider<PageController>((_) => PageController());
+final mainPageScaffoldKeyProvider =
+    StateProvider<GlobalKey<ScaffoldState>>((_) => GlobalKey<ScaffoldState>());
+final pageNames = ["Home", "Wallets", "Analysis"];
+final pages = <Widget>[
+  const KeepAlivePage(child: Homepage()),
+  const KeepAlivePage(child: Homepage()),
+  const KeepAlivePage(child: Homepage()),
+];
 
 class KeuanganKuPro extends HookConsumerWidget {
   const KeuanganKuPro({super.key});
 
-  void whenBottomNavbarChanged(int value, WidgetRef ref, PageController pageController) {
+  void whenBottomNavbarChanged(
+      int value, WidgetRef ref, PageController pageController) {
     ref.read(pageIndexProvider.notifier).state = value;
     pageController.jumpToPage(value);
   }
 
-  List<BottomNavigationBarItem> bottomNavigationBarItems() {
+  Color _iconColor(index, currenIndex) {
+    if (index != currenIndex) return FontColor.black.getColor().withAlpha(150);
+    return FontColor.black.getColor();
+  }
+
+  List<BottomNavigationBarItem> bottomNavigationBarItems(currentIndex) {
     return [
-      BottomNavigationBarItem(icon: const Icon(Icons.home), label: pageNames[0]),
-      BottomNavigationBarItem(icon: const Icon(Icons.wallet), label: pageNames[1])
+      BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            color: _iconColor(0, currentIndex),
+          ),
+          label: pageNames[0]),
+      BottomNavigationBarItem(
+          icon: Icon(
+            Icons.wallet,
+            color: _iconColor(1, currentIndex),
+          ),
+          label: pageNames[1]),
+      BottomNavigationBarItem(
+          icon: Icon(
+            FluentIcons.data_usage_20_filled,
+            color: _iconColor(2, currentIndex),
+          ),
+          label: pageNames[2]),
     ];
   }
 
-  void initData(WidgetRef ref){
+  void initData(WidgetRef ref) {
     ref.watch(globalWalletsProvider.notifier).initData();
     ref.watch(globalIncomeCategoriesProvider.notifier).initData();
     ref.watch(globalExpenseCategoriesProvider.notifier).initData();
@@ -45,14 +71,12 @@ class KeuanganKuPro extends HookConsumerWidget {
     var scaffoldKey = ref.watch(mainPageScaffoldKeyProvider);
     var pageIndex = ref.watch(pageIndexProvider);
     var pageController = ref.watch(pageControllerProvider);
-    var pages = ref.watch(pagesProvider);
 
     initData(ref);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(pageNames[pageIndex]),
-        backgroundColor: BackgroundColor.white.getColor(),
       ),
       key: scaffoldKey,
       body: PageView(
@@ -65,7 +89,10 @@ class KeuanganKuPro extends HookConsumerWidget {
         currentIndex: pageIndex,
         onTap: (value) => whenBottomNavbarChanged(value, ref, pageController),
         backgroundColor: Colors.white,
-        items: bottomNavigationBarItems(),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: FontColor.black.getColor(),
+        unselectedItemColor: FontColor.black.getColor().withAlpha(150),
+        items: bottomNavigationBarItems(pageIndex),
       ),
     );
   }

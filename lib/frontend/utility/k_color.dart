70170efd4c1d467c708b3c1aea5dac23befcb249
andreas64 <derzandrr03@gmail.com>
2024-08-16 07:55:45 +0700

    add generateSoftColor, generateSoftPalette, generatePastelPalette, generatePastelColor

--- lib/frontend/utility/k_color.dart
+++ lib/frontend/utility/k_color.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 enum BackgroundColor {
@@ -36,4 +37,47 @@ List<Color> generate3Color(Color mainColor){
     Color(mainColor.value + 1249810),
     Color(mainColor.value + (2* 0x101797))
   ];
+}
+
+Color generatePastelColor() {
+  final Random random = Random();
+  final int r = (random.nextInt(256) + 255) ~/ 2;
+  final int g = (random.nextInt(256) + 255) ~/ 2;
+  final int b = (random.nextInt(256) + 255) ~/ 2;
+
+  return Color.fromARGB(255, r, g, b);
+}
+
+List<Color> generatePastelPalette(int count) {
+  final List<Color> colors = [];
+  final double hueStep = 360 / count;
+  final Random random = Random();
+
+  for (int i = 0; i < count; i++) {
+    final double hue = i * hueStep + random.nextDouble() * hueStep / 2;
+    final HSLColor hslColor = HSLColor.fromAHSL(1.0, hue, 0.7, 0.9);
+    colors.add(hslColor.toColor());
+  }
+
+  return colors;
+}
+
+Color generateSoftColor() {
+  final Random random = Random();
+  final int r = (random.nextInt(156) + 100); // Nilai r antara 100-255
+  final int g = (random.nextInt(156) + 100); // Nilai g antara 100-255
+  final int b = (random.nextInt(156) + 100); // Nilai b antara 100-255
+
+  return Color.fromARGB(255, r, g, b);
+}
+
+List<Color> generateSoftPalette(int count) {
+  final List<Color> colors = [];
+
+  for (int i = 0; i < count; i++) {
+    final Color color = generateSoftColor();
+    colors.add(color);
+  }
+
+  return colors;
 }

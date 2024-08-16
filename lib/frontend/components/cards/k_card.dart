d93785515666ae6b56c44429ecc316911c432768
andreas64 <derzandrr03@gmail.com>
2024-08-16 08:27:18 +0700

    update k_card.dart and k_card_plus.dart

--- lib/frontend/components/cards/k_card.dart
+++ lib/frontend/components/cards/k_card.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:keuanganku/frontend/components/text/k_text.dart';
+import 'package:keuanganku/frontend/utility/k_color.dart';
 
-Widget KCard(Widget child, Color color){
+Widget KCard(BuildContext context,
+    {required Widget child, required String title, Color? color, Icon? icon}) {
+  TextStyle textStyle = getTextStyle(context, KTStyle.title, KTSType.medium, FontColor.black.color);
   return Container(
-    width: double.infinity,
     decoration: BoxDecoration(
-      borderRadius: const BorderRadius.all(Radius.circular(10)),
-      color: color
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        BoxShadow(
+          color:
+              Colors.black.withOpacity(0.1), // Warna bayangan dengan opasitas
+          offset: const Offset(0, 0.5), // Posisi bayangan
+          blurRadius: 1, // Jarak blur bayangan
+          spreadRadius: 0.5, // Penyebaran bayangan
+        ),
+      ],
     ),
-    padding: const EdgeInsets.all(20),
-    child: child,
+    child: Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(title, style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: textStyle.fontSize,
+            fontFamily: textStyle.fontFamily,
+            color: textStyle.color
+          )),
+          child,
+        ],
+      ),
+    ),
   );
 }

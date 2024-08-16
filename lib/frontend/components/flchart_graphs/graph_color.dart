3564d10105e9c4c6933685e22d440d7fb8a76b4c
andreas64 <derzandrr03@gmail.com>
2024-08-16 07:58:07 +0700

    add new colors

--- lib/frontend/components/flchart_graphs/graph_color.dart
+++ lib/frontend/components/flchart_graphs/graph_color.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 enum KGraphColor {
-  red(Color(0xFFFF2C2C));
+  red(Color(0xFFFF2C2C)),
+  pastel_green(Color(0xFF79AC78)),
+  pastel_red(Color(0xFFC84361));
   const KGraphColor(this.color);
   final Color color;
 }

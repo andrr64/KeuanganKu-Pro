57e8eec5455e5f79608cffba99aa3c1bb6ba12b5
andreas64 <derzandrr03@gmail.com>
2024-08-16 08:29:04 +0700

    update analysis.dart + analysis_provider.dart -> implement analysis_provider.dart

--- lib/frontend/app/main/analysis/analysis.dart
+++ lib/frontend/app/main/analysis/analysis.dart
@@ -1,47 +1,231 @@
-import 'dart:math';
-
 import 'package:fl_chart/fl_chart.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:keuanganku/frontend/app/expense_category_provider.dart';
+import 'package:keuanganku/frontend/app/main/analysis/analysis_provider.dart';
 import 'package:keuanganku/frontend/app/main/page_padding.dart';
+import 'package:keuanganku/frontend/components/bullet.dart';
+import 'package:keuanganku/frontend/components/buttons/k_button.dart';
 import 'package:keuanganku/frontend/components/cards/k_card.dart';
 import 'package:keuanganku/frontend/components/flchart_graphs/bar_weekly.dart';
 import 'package:keuanganku/frontend/components/flchart_graphs/graph_color.dart';
 import 'package:keuanganku/frontend/components/flchart_graphs/piecart.dart';
+import 'package:keuanganku/frontend/components/text/k_text.dart';
+import 'package:keuanganku/frontend/components/utility/currency_format.dart';
+import 'package:keuanganku/frontend/components/utility/space_x.dart';
 import 'package:keuanganku/frontend/components/utility/space_y.dart';
 import 'package:keuanganku/frontend/utility/k_color.dart';
 
 class AnalysisPage extends HookConsumerWidget {
-  int currentIndex = 0;
+  const AnalysisPage({super.key});
 
-  AnalysisPage({super.key});
   List<BarChartGroupData> data_mingguan(BuildContext context) {
     return [
-      weeklyBarData(context, 0, 10, barColor: KGraphColor.red.color),
-      weeklyBarData(context, 1, 15, barColor: KGraphColor.red.color),
-      weeklyBarData(context, 2, 13, barColor: KGraphColor.red.color),
-      weeklyBarData(context, 3, 40, barColor: KGraphColor.red.color),
-      weeklyBarData(context, 4, 20, barColor: KGraphColor.red.color),
-      weeklyBarData(context, 5, 20, barColor: KGraphColor.red.color),
-      weeklyBarData(context, 6, 10, barColor: KGraphColor.red.color),
+      weeklyBarData(context, 0, 10, barColor: KGraphColor.pastel_red.color),
+      weeklyBarData(context, 1, 15, barColor: KGraphColor.pastel_red.color),
+      weeklyBarData(context, 2, 13, barColor: KGraphColor.pastel_red.color),
+      weeklyBarData(context, 3, 40, barColor: KGraphColor.pastel_red.color),
+      weeklyBarData(context, 4, 20, barColor: KGraphColor.pastel_red.color),
+      weeklyBarData(context, 5, 20, barColor: KGraphColor.pastel_red.color),
+      weeklyBarData(context, 6, 10, barColor: KGraphColor.pastel_red.color),
     ];
-  }
-  List<KPieSectionData> data_donut(){
-    return List.generate(10, (index){
-      return KPieSectionData(
-          value: Random().nextInt(200).toDouble(),
-          index: index,
-          title: index.toString(),
-          color: Colors.red,
-          isTouched: currentIndex == index,
-          radius: 20,
-          whenTouched: () => currentIndex = index
-      );
-    });
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Widget expenseByCategoryGraph() {
+      final expenseByCategoryGraphProvider =
+          ref.watch(analysisPageExpenseByCategoryProvider);
+
+      Widget buildLegends() {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: List.generate(
+              expenseByCategoryGraphProvider.textLegends.length, (index) {
+            return Row(
+              children: [
+                Container(
+                  color: expenseByCategoryGraphProvider.sectionColors[index],
+                  height: 10,
+                  width: 10,
+                ),
+                dummyWidth(5),
+                SizedBox(
+                  width: vw(context, 27.5), // Atur lebar sesuai kebutuhan
+                  child: expenseByCategoryGraphProvider.textLegends[index],
+                ),
+              ],
+            );
+          }),
+        );
+      }
+
+      return KCard(
+        context,
+        title: 'Expense by Category',
+        color: BaseColor.old_red.color,
+        child: Padding(
+            padding: const EdgeInsets.only(top: 20, left: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: vh(context, 22.5),
+                      width: vw(context, 40),
+                      child: expenseByCategoryGraphProvider.loading
+                          ? const Center(child: CircularProgressIndicator())
+                          : KPieChart(
+                              sections:
+                                  expenseByCategoryGraphProvider.pieChart),
+                    ),
+                    const SizedBox(
+                        width:
+                            25), // Menambahkan jarak antara grafik dan legenda
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          kText(
+                              context,
+                              'Total',
+                              KTStyle.title,
+                              fontWeight: FontWeight.w600,
+                              KTSType.medium),
+                          kText(
+                              context,
+                              currencyFormat(
+                                  expenseByCategoryGraphProvider.total),
+                              KTStyle.title,
+                              KTSType.medium),
+                          dummyHeight(10),
+                          buildLegends(),
+                          dummyHeight(10),
+                          OutlinedButton(
+                              onPressed: () {}, child: Text('Detail'))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      );
+    }
+
+    Widget expenseBarGraph() {
+      return KCard(
+        context,
+        title: 'Weekly Expense',
+        color: BaseColor.old_red.color,
+        child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                dummyHeight(20),
+                SizedBox(
+                  height: 225,
+                  child: WeeklyBarChart(
+                    context,
+                    barGroups: data_mingguan(context),
+                    maxY: findBarChartValue(data_mingguan(context)),
+                  ),
+                ),
+                dummyHeight(20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            KBullet(10, color: KGraphColor.pastel_red.color),
+                            dummyWidth(10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                kText(context, 'Total', KTStyle.title,
+                                    KTSType.small,
+                                    fontWeight: FontWeight.w600),
+                                kText(context, '${currencyFormat(120000000)}',
+                                    KTStyle.label, KTSType.small),
+                              ],
+                            )
+                          ],
+                        ),
+                        dummyHeight(10),
+                        Row(
+                          children: [
+                            KBullet(10),
+                            dummyWidth(10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Average',
+                                  style: getTextStyle(context, KTStyle.title,
+                                      KTSType.small, FontColor.black.color,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                kText(context, '${currencyFormat(120000000)}',
+                                    KTStyle.label, KTSType.small),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            KBullet(10, color: KGraphColor.pastel_red.color),
+                            dummyWidth(10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                kText(context, 'Highest', KTStyle.title,
+                                    KTSType.small,
+                                    fontWeight: FontWeight.w600),
+                                kText(context, '${currencyFormat(120000000)}',
+                                    KTStyle.label, KTSType.small),
+                              ],
+                            )
+                          ],
+                        ),
+                        dummyHeight(10),
+                        Row(
+                          children: [
+                            KBullet(10),
+                            dummyWidth(10),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Lowest',
+                                  style: getTextStyle(context, KTStyle.title,
+                                      KTSType.small, FontColor.black.color,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                kText(context, '${currencyFormat(120000000)}',
+                                    KTStyle.label, KTSType.small),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            )),
+      );
+    }
+
     Widget content() {
       return PagePadding(
         context,
@@ -49,32 +233,20 @@ class AnalysisPage extends HookConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            dummyHeight(10),
-            KCard(
-              context,
-              title: 'Grafik Mingguan',
-              color: BaseColor.old_red.color,
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: SizedBox(
-                    height: 250,
-                    child: WeeklyBarChart(
-                      context,
-                      barGroups: data_mingguan(context),
-                      maxY: findBarChartValue(data_mingguan(context)),
-                    ),
-                  )),
-            ),
-            dummyHeight(30),
-            KCard(context,
-                title: 'Kategori Pengeluaran',
-                color: BaseColor.old_red.color,
-                child: Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: SizedBox(
-                      height: 250,
-                      child: KPieChart(sections: data_donut()),
-                    )))
+            expenseBarGraph(),
+            dummyHeight(15),
+            expenseByCategoryGraph(),
+            ElevatedButton(
+                onPressed: () {
+                  ref
+                      .watch(analysisPageExpenseByCategoryProvider.notifier)
+                      .updateData(
+                          ref
+                              .watch(globalExpenseCategoriesProvider.notifier)
+                              .getById,
+                          context);
+                },
+                child: Text('Hola'))
           ],
         ),
       );
---
+++ lib/frontend/app/main/analysis/analysis_provider.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:keuanganku/backend/api/expense.dart';
+import 'package:keuanganku/backend/database/model/expense_category.dart';
+import 'package:keuanganku/enum/date_range.dart';
+import 'package:keuanganku/frontend/components/flchart_graphs/piecart.dart';
+import 'package:keuanganku/frontend/components/text/k_text.dart';
+import 'package:keuanganku/frontend/components/utility/space_x.dart';
+import 'package:keuanganku/frontend/utility/k_color.dart';
+
+class AnalysisPageData {
+  final List<KPieSectionData> expensePieChartByCategory;
+  final List<Widget>? expensePieChartLegends;
+  final DateRange expensePieChartByCategoryDateRange;
+  final DateRange expenseBarChartDataRange;
+
+  AnalysisPageData({
+    this.expensePieChartByCategoryDateRange = DateRange.month,
+    this.expenseBarChartDataRange = DateRange.week,
+    this.expensePieChartLegends,
+    required this.expensePieChartByCategory,
+  });
+
+  AnalysisPageData copyWith({
+    List<KPieSectionData>? pieSectionsData,
+    DateRange? pieChartDateRange,
+    List<Widget>? pieLegendsData,
+    DateRange? barChartDataRange,
+  }) {
+    return AnalysisPageData(
+      expensePieChartLegends: pieLegendsData ?? this.expensePieChartLegends,
+      expensePieChartByCategory: pieSectionsData ?? this.expensePieChartByCategory,
+      expensePieChartByCategoryDateRange: pieChartDateRange?? this.expensePieChartByCategoryDateRange,
+      expenseBarChartDataRange: barChartDataRange?? this.expenseBarChartDataRange,
+    );
+  }
+}
+class AnalysisPageProvider extends Notifier<AnalysisPageData> {
+  bool _init = false;
+
+  @override
+  AnalysisPageData build() => AnalysisPageData(expensePieChartByCategory: []);
+
+  Future<void> initData(DBModelExpenseCategory Function(int) expenseCategoryGetter) async {
+    if (!_init) {
+      await updateExpensePieChartByCategoryData(expenseCategoryGetter);
+      _init = true;
+    }
+  }
+
+  Future<void> updateExpensePieChartByCategoryData(DBModelExpenseCategory Function(int) expenseCategoryGetter) async {
+    final newData = await APIExpenseData.getExpenseTotalByCategories(expenseCategoryGetter: expenseCategoryGetter);
+    final colors = generateSoftPalette(newData.length);
+    final double totalExpense = newData.fold(0, (sum, item) => sum + item.total);
+
+    final sections = newData.asMap().entries.map((entry) {
+      final i = entry.key;
+      final item = entry.value;
+      final percentage = (item.total / totalExpense) * 100;
+      return KPieSectionData(
+        value: item.total,
+        index: i,
+        title: "${percentage.toStringAsFixed(1)}%",
+        color: colors[i],
+        isTouched: true,
+        radius: 27.5,
+        whenTouched: () => (),
+      );
+    }).toList();
+
+    final legends = newData.asMap().entries.map((entry) {
+      final i = entry.key;
+      final item = entry.value;
+      final percentage = (item.total / totalExpense) * 100;
+      return Row(
+        children: [
+          Container(color: colors[i], width: 10, height: 10,),
+          dummyWidth(5),
+          Text('${item.category.name!} ${percentage.toStringAsFixed(1)}%'),
+        ],
+      );
+    }).toList();
+    state = state.copyWith(
+      pieSectionsData: sections,
+      pieLegendsData: legends,
+    );
+  }
+}
+final analysisPageProvider = NotifierProvider<AnalysisPageProvider, AnalysisPageData>(AnalysisPageProvider.new);
+
+class AnalysisPageExpenseByCategoryData {
+  final List<KPieSectionData> pieChart;
+  final List<Color> sectionColors;
+  final List<Widget> textLegends;
+  final DateRange dateRange;
+  final bool loading;
+  final double total;
+
+  AnalysisPageExpenseByCategoryData({
+    this.pieChart = const [],
+    this.total = 0,
+    this.textLegends = const [],
+    this.dateRange = DateRange.month,
+    this.loading = false,
+    this.sectionColors = const []
+  });
+
+  AnalysisPageExpenseByCategoryData copyWith({
+    List<KPieSectionData>? pieChart,
+    List<Widget>? legends,
+    DateRange? dateRange,
+    bool? loading,
+    double? total,
+    List<Color>? sectionColors
+  }) {
+    return AnalysisPageExpenseByCategoryData(
+      pieChart: pieChart ?? this.pieChart,
+      total: total?? this.total,
+      sectionColors: sectionColors?? this.sectionColors,
+      textLegends: legends ?? this.textLegends,
+      dateRange: dateRange ?? this.dateRange,
+      loading: loading ?? this.loading,
+    );
+  }
+}
+class AnalysisPageExpenseByCategoryProvider extends Notifier<AnalysisPageExpenseByCategoryData>{
+  bool _init = false;
+
+  @override
+  AnalysisPageExpenseByCategoryData build() => AnalysisPageExpenseByCategoryData();
+  Future<void> initData(BuildContext context, {required DBModelExpenseCategory Function(int) expenseCategoryGetter}) async {
+    if (!_init){
+      await updateData(expenseCategoryGetter, context);
+      _init = true;
+    }
+  }
+
+  Future<void> updateData(DBModelExpenseCategory Function(int) expenseCategoryGetter, BuildContext context) async {
+    state.copyWith(loading: true);
+
+    final newData = await APIExpenseData.getExpenseTotalByCategories(expenseCategoryGetter: expenseCategoryGetter);
+    final colors = generateSoftPalette(newData.length);
+    final double totalExpense = newData.fold(0, (sum, item) => sum + item.total);
+
+    final sections = newData.asMap().entries.map((entry) {
+      final i = entry.key;
+      final item = entry.value;
+      final percentage = (item.total / totalExpense) * 100;
+      return KPieSectionData(
+        value: item.total,
+        index: i,
+        title: "${percentage.toStringAsFixed(1)}%",
+        color: colors[i],
+        isTouched: true,
+        radius: 30,
+        whenTouched: () => (),
+      );
+    }).toList();
+
+    final legends = newData.asMap().entries.map((entry) {
+      final i = entry.key;
+      final item = entry.value;
+      final percentage = (item.total / totalExpense) * 100;
+      return  Text(
+        '${item.category.name!} (${percentage.toStringAsFixed(1)}%)',
+        overflow: TextOverflow.clip,
+        style: getTextStyle(context, KTStyle.label, KTSType.medium, FontColor.black.color),);
+    }).toList();
+    state = state.copyWith(
+      pieChart: sections,
+      legends: legends,
+      sectionColors: colors,
+      total: totalExpense,
+      loading: false
+    );
+  }
+}
+final analysisPageExpenseByCategoryProvider =
+NotifierProvider<AnalysisPageExpenseByCategoryProvider, AnalysisPageExpenseByCategoryData>
+  (AnalysisPageExpenseByCategoryProvider.new);

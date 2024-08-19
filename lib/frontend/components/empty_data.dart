67d8f3289fdc66aa6df35ef7114ae79da8030e10
andreas64 <derzandrr03@gmail.com>
2024-08-19 23:05:48 +0700

    add empty data widget

---
+++ lib/frontend/components/empty_data.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:keuanganku/frontend/components/text/k_text.dart';
+import 'package:keuanganku/frontend/components/utility/space_y.dart';
+import 'package:keuanganku/frontend/utility/k_color.dart';
+
+class EmptyData extends StatelessWidget {
+  final IconData? iconData;
+  EmptyData({super.key, this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 32),
+      child: Center(
+          child: Column(
+            children: [
+              Icon(iconData?? Icons.sentiment_satisfied, size: 64, color: FontColor.black.color,),
+              dummyHeight(5),
+              kText(context, 'Empty Data.', KTStyle.label, KTSType.large)
+            ],
+          )
+      ),
+    );
+  }
+}

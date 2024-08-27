8c9f6e752772a017558c2405cf5d94278be234fa
andreas64 <derzandrr03@gmail.com>
2024-08-27 08:28:25 +0700

    update font family

--- lib/frontend/themes/light_theme.dart
+++ lib/frontend/themes/light_theme.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:keuanganku/frontend/utility/k_color.dart';
 
 const Color BLACK_FONT_COLOR = Color(0xff14192b);
+const String FONT_FAMILY = 'OpenSans';
 
 ThemeData light_theme = ThemeData(
     textTheme: const TextTheme(
-      displayLarge: TextStyle(fontFamily: 'OpenSans'),
-      displayMedium: TextStyle(fontFamily: 'OpenSans'),
-      displaySmall: TextStyle(fontFamily: 'OpenSans'),
-      bodyLarge: TextStyle(fontFamily: 'OpenSans'),
-      bodyMedium: TextStyle(fontFamily: 'OpenSans'),
-      bodySmall: TextStyle(fontFamily: 'OpenSans'),
-      titleLarge: TextStyle(fontFamily: 'OpenSans'),
-      titleMedium: TextStyle(fontFamily: 'OpenSans'),
-      titleSmall: TextStyle(fontFamily: 'OpenSans'),
-      labelLarge: TextStyle(fontFamily: 'OpenSans'),
-      labelMedium: TextStyle(fontFamily: 'OpenSans'),
-      labelSmall: TextStyle(fontFamily: 'OpenSans'),
+      displayLarge: TextStyle(fontFamily: FONT_FAMILY),
+      displayMedium: TextStyle(fontFamily: FONT_FAMILY),
+      displaySmall: TextStyle(fontFamily: FONT_FAMILY),
+      bodyLarge: TextStyle(fontFamily: FONT_FAMILY),
+      bodyMedium: TextStyle(fontFamily: FONT_FAMILY),
+      bodySmall: TextStyle(fontFamily: FONT_FAMILY),
+      titleLarge: TextStyle(fontFamily: FONT_FAMILY),
+      titleMedium: TextStyle(fontFamily: FONT_FAMILY),
+      titleSmall: TextStyle(fontFamily: FONT_FAMILY),
+      labelLarge: TextStyle(fontFamily: FONT_FAMILY),
+      labelMedium: TextStyle(fontFamily: FONT_FAMILY),
+      labelSmall: TextStyle(fontFamily: FONT_FAMILY),
     ),
     brightness: Brightness.light,
     scaffoldBackgroundColor: Colors.white,

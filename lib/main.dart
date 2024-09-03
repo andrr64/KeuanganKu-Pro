92afffb226304ee6bf251e1307fb0989de30573e
andreas64 <derzandrr03@gmail.com>
2024-09-04 00:19:30 +0700

    remove deviceInfo code and appTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:keuanganku/backend/database/database_services.dart';
-import 'package:keuanganku/device_info.dart';
 import 'package:keuanganku/frontend/app/app.dart';
 import 'package:keuanganku/frontend/app/content_when_x.dart';
 import 'package:keuanganku/frontend/themes/light_theme.dart';
 
 DatabaseServices db = DatabaseServices();
-late final ThemeData appTheme;
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,9 +35,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    DEVICE_initData(context);
-    appTheme = Theme.of(context);
-
     return MaterialApp(
       title: 'KeuanganKu Pro',
       home: const KeuanganKuPro(),

547d256f4125a6277eae892838dec28c25fd9c93
andreas64 <derzandrr03@gmail.com>
2024-09-08 20:43:44 +0700

    update expense_limiter helper and model

--- lib/backend/database/helper/expense_limiter.dart
+++ lib/backend/database/helper/expense_limiter.dart
@@ -1,8 +1,8 @@
-import 'package:keuanganku/backend/database/helper/expense.dart';
+import 'package:keuanganku/backend/database/helper/expense_category.dart';
 import 'package:keuanganku/backend/database/helper/helper.dart';
+import 'package:keuanganku/backend/database/model/expense_category.dart';
 import 'package:keuanganku/backend/database/model/expense_limiter.dart';
 import 'package:keuanganku/backend/database/utility/table_column_generator.dart';
-import 'package:keuanganku/enum/date_range.dart';
 import 'package:keuanganku/main.dart';
 
 class DBHelperExpenseLimiter extends DBHelper<DBModelExpenseLimiter> {
@@ -16,7 +16,11 @@ class DBHelperExpenseLimiter extends DBHelper<DBModelExpenseLimiter> {
 
   @override
   Future<int> insert({required DBModelExpenseLimiter data}) async{
-    return await db.database.insert(tableName, data.toJson());
+    try {
+      return await db.database.insert(tableName, data.toJson());
+    } catch (e){
+      throw Exception(e);
+    }
   }
 
   @override
@@ -29,11 +33,9 @@ class DBHelperExpenseLimiter extends DBHelper<DBModelExpenseLimiter> {
     List<DBModelExpenseLimiter> result = [];
     for (final e in query_result){
       DBModelExpenseLimiter x = modelGenerator.fromJson(e);
-      final expenses_amount = await DBHelperExpense().readWithWhereClause(
-        where: 'datetime >= ? AND datetime <= ? AND category_id = ?', 
-        whereArgs: [x.period.startDateISO8601, x.period.endDateISO8601, x.category_id]
-      );
-      x.current_amount = expenses_amount.fold(0.0, (sum, i) => sum + i.amount!);
+      DBModelExpenseCategory category = await DBHelperExpenseCategory().readById(id: int.parse(e['category_id'].toString()));
+      x.category = category;
+      await x.updateCurrentAmount();
       result.add(x);
     }
     return result;
@@ -41,24 +43,15 @@ class DBHelperExpenseLimiter extends DBHelper<DBModelExpenseLimiter> {
 
   @override
   Future<DBModelExpenseLimiter> readById({required int id}) async {
-    final query_result = await db.database.query(tableName, where: 'id = ?', whereArgs: [id]);
-    if (query_result.isEmpty){
-      throw Exception('Empty data');
-    }
-    DBModelExpenseLimiter data = DBModelExpenseLimiter().fromJson(query_result.first);
-    if (query_result.isEmpty) throw Exception('No data found for the given ID.');
-    final expense_amount = await DBHelperExpense().readWithWhereClause(
-      where: 'datetime >= ? AND datetime <= ? AND category_id = ?', 
-      whereArgs: [data.period.startDateISO8601, data.period.endDateISO8601, data.category_id]
-    );
-    data.current_amount = expense_amount.fold(0.0, (sum, i) => sum + i.amount!);
-    return data;
+    ///TODO: implement db.helper.expenselimiter.readById
+    throw UnimplementedError();
   }
   
   @override
   List<Map<String, String>> get tableColumns => [
     createSql3Column(name: 'id', dtype: 'INTEGER', primary: true, required: true),
     createSql3Column(name: 'category_id', dtype: 'INTEGER', required: true),
+    createSql3Column(name: 'wallet_id', dtype: 'INTEGER', required: true),
     createSql3Column(name: 'limit_amount', dtype: 'DOUBLE', required: true),
     createSql3Column(name: 'period_id', dtype: 'INTEGER', required: true)
   ];
--- lib/backend/database/model/expense_limiter.dart
+++ lib/backend/database/model/expense_limiter.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/foundation.dart';
+import 'package:keuanganku/backend/database/helper/expense.dart';
+import 'package:keuanganku/backend/database/model/expense_category.dart';
 import 'package:keuanganku/backend/database/model/model.dart';
 import 'package:keuanganku/enum/date_range.dart';
 
 class DBModelExpenseLimiter extends DBModel {
   int id;
-  int category_id;
+  int wallet_id;
   double limit_amount;
   int period_id;
   double current_amount;
+  DBModelExpenseCategory? category;
 
   DBModelExpenseLimiter({
     this.id = 0,
-    this.category_id = 0,
     this.limit_amount = 0,
     this.period_id = 0,
     this.current_amount = 0,
+    this.wallet_id = 0,
+    this.category
   });
 
   DateRange get period {
@@ -33,16 +37,42 @@ class DBModelExpenseLimiter extends DBModel {
   fromJson(Map<String, dynamic> json) {
     return DBModelExpenseLimiter(
       id: json['id'],
-      category_id: json['category_id'],
+      wallet_id: json['wallet_id'],
       limit_amount: json['limit_amount'],
       period_id: json['period_id'],
+      category: json['category']
     );
+  }
+  
+  Future<void> updateCurrentAmount() async {
+    var expenses_amount = [];
+    if (wallet_id == 0){
+      String where = 'datetime >= ? AND datetime <= ? AND category_id = ?'; 
+      expenses_amount = await DBHelperExpense().readWithWhereClause(
+        where: where, whereArgs: [period.startDateISO8601, period.endDateISO8601, category!.id]);
+    } else {
+      String where = 'datetime >= ? AND datetime <= ? AND category_id = ? AND wallet_id = ?'; 
+      List whereArgs = [period.startDateISO8601, period.endDateISO8601, category!.id, wallet_id];
+      expenses_amount = await DBHelperExpense().readWithWhereClause(
+          where: where, 
+          whereArgs: whereArgs
+      );
+    }
+    current_amount = expenses_amount.fold(0.0, (sum, e) => sum + e.amount!);
+  }
+
+  void addCurrentAmount(double amount) => current_amount += amount;
+
+  Future<bool> updateData(){
+    //TODO: model.expenseLimiter.updateData
+    throw UnimplementedError();
   }
 
   @override
   Map<String, dynamic> toJson() {
     return {
-      'category_id': category_id,
+      'category_id': category == null? -1 : category!.id,
+      'wallet_id': wallet_id,
       'limit_amount': limit_amount,
       'period_id': period_id,
     };

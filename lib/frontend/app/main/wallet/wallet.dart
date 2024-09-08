9f61f4a04a92cff81d2a459058ee75d48f612be7
andreas64 <derzandrr03@gmail.com>
2024-09-08 20:42:04 +0700

    add wallet page

---
+++ lib/frontend/app/main/wallet/wallet.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:keuanganku/backend/database/model/expense_limiter.dart';
+import 'package:keuanganku/frontend/app/main/page_padding.dart';
+import 'package:keuanganku/frontend/app/providers/expense_category_provider.dart';
+import 'package:keuanganku/frontend/app/providers/expense_limiter.dart';
+import 'package:keuanganku/frontend/app/providers/wallet_provider.dart';
+import 'package:keuanganku/frontend/components/cards/balance_card.dart';
+import 'package:keuanganku/frontend/components/cards/expense_limiter_card.dart';
+import 'package:keuanganku/frontend/components/utility/space_y.dart';
+
+INITDATA_WalletPage(BuildContext context, WidgetRef ref) async {
+  
+}
+
+class WalletPage extends HookConsumerWidget {
+  const WalletPage({super.key});
+
+  void callbackWhenDataSaved(WidgetRef ref, DBModelExpenseLimiter newExpenseLimiter){
+    ref.read(globalExpenseLimiterNotifierProvider.notifier).add(newExpenseLimiter);
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final walletsProvider = ref.watch(globalWalletsProvider);
+    final expenseCategoryProvider = ref.watch(globalExpenseCategoriesProvider);
+    final expenseLimiterProvider = ref.watch(globalExpenseLimiterNotifierProvider);
+
+    return SingleChildScrollView(
+      child: PagePadding(
+        context, 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const BalanceCard( without_title: true, without_name: true,),
+            dummyHeight(10),
+            ExpenseLimiterCard(
+              callbackWhenNewLimiterSaved: (newExpenseLimiter){
+                print(newExpenseLimiter.category!.name);
+                callbackWhenDataSaved(ref, newExpenseLimiter);
+              },
+              limiter_data: expenseLimiterProvider, 
+              expenseCategories: expenseCategoryProvider, 
+              wallets: walletsProvider,
+            ),
+            ElevatedButton(onPressed: () async{
+
+            }, child: const Text('kick me'))
+          ],
+        ),
+      ),
+    );
+  }
+}

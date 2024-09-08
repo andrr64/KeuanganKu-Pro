02b55427bcfcdfa0b88f79c6fc5b68e2350f7994
andreas64 <derzandrr03@gmail.com>
2024-09-08 20:41:34 +0700

    add app.providers.expense_limiter.dart

---
+++ lib/frontend/app/providers/expense_limiter.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:keuanganku/backend/database/helper/expense_limiter.dart';
+import 'package:keuanganku/backend/database/model/expense_limiter.dart';
+
+final globalExpenseLimiterNotifierProvider = NotifierProvider<ExpenseLimiterProvider, List<DBModelExpenseLimiter>>(ExpenseLimiterProvider.new);
+
+class ExpenseLimiterProvider extends Notifier<List<DBModelExpenseLimiter>>{
+  bool init = false;
+  
+  @override
+  List<DBModelExpenseLimiter> build() => [];
+
+  void initData() async {
+    if (!init){
+      state = await DBHelperExpenseLimiter().readAll(); 
+      init = true;
+    }
+  }
+
+  void add(DBModelExpenseLimiter limiter){
+    state = [...state, limiter];
+  }
+}
